f88052c2e11f65f907e5c4e2a585fe96c2d6771e
joshkaye88 <jkaye@shaw.ca>
2025-06-20 09:48:36 -0600

    fix transaction modal close bug, impplement loader

---
+++ src/components/Loader/loader.ts
@@ -0,0 +1,12 @@
+import Qry from "../../utils/Qry";
+
+export class Loader {
+
+    public static remove() {
+        const loader = Qry.one<HTMLElement>('page-loader')
+        if( loader ) {
+            loader.classList.add('opacity-0')
+            setTimeout( () => loader.remove(), 250 )
+        }
+    }
+}
--- src/utils/loader.ts
+++
@@ -1,9 +0,0 @@
-import Qry from "./Qry";
-
-export function removeLoader() {
-    const loader = Qry.one<HTMLElement>('page-loader')
-    if( loader ) {
-        loader.classList.add('opacity-0')
-        setTimeout( () => loader.remove(), 200 )
-    }
-}
