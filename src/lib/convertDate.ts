d4e77874c1b34f6d97d4cb83cd54537ce9a1ebd9
joshkaye88 <jkaye@shaw.ca>
2025-10-11 20:19:00 -0600

    save before restructuring

--- src/lib/convertDate.ts
+++ src/lib/convertDate.ts
@@ -5,6 +5,10 @@ type FormattedDates = {
     [key in ValidDateFormats]: string
 }
 
+const shortMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+const fullMonthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 export const convertDate = (inputDate: string | undefined | null = undefined) => {
 
     const date = inputDate
@@ -15,26 +19,22 @@ export const convertDate = (inputDate: string | undefined | null = undefined) =>
         throw new Error(`Invalid date: ${inputDate}`)
     }
 
-    const shortMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-    const fullMonthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const monthNumber = String(month + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+
+    const formattedDates : FormattedDates = {
+        'MMM-DD' : `${shortMonthNames[month]} ${day}`,
+        'MMM-YYYY' : `${shortMonthNames[month]} ${year}`,
+        'MMM-DD-YYYY' : `${shortMonthNames[month]} ${day}, ${year}`,
+        'MMMM-YYYY' : `${fullMonthNames[month]} ${year}`,
+        'YYYY-MM' : `${year}-${monthNumber}`,
+        'YYYY-MM-DD' : `${year}-${monthNumber}-${day}`,
+    }
 
     return {
         to( format : ValidDateFormats ) {
-   
-            const year = date.getFullYear()
-            const month = date.getMonth()
-            const monthNumber = String(month + 1).padStart(2, '0')
-            const day = String(date.getDate()).padStart(2, '0')
-
-            const formattedDates : FormattedDates = {
-                'MMM-DD' : `${shortMonthNames[month]} ${day}`,
-                'MMM-YYYY' : `${shortMonthNames[month]} ${year}`,
-                'MMM-DD-YYYY' : `${shortMonthNames[month]} ${day}, ${year}`,
-                'MMMM-YYYY' : `${fullMonthNames[month]} ${year}`,
-                'YYYY-MM' : `${year}-${monthNumber}`,
-                'YYYY-MM-DD' : `${year}-${monthNumber}-${day}`,
-            }
-
             return formattedDates[format]
         },
        
