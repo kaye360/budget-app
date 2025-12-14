ed2f92df7f638b2ccbe58a4499ef1042bca16d44
joshkaye88 <jkaye@shaw.ca>
2025-12-13 22:37:28 -0700

    update Dashboard and graph UI and dates

--- src/lib/convertDate.ts
+++ src/lib/convertDate.ts
@@ -1,5 +1,5 @@
 
-type ValidDateFormats = 'YYYY-MM' | 'YYYY-MM-DD' | "MMM-DD" | "MMM-YYYY" |"MMM-DD-YYYY" | 'MMMM-YYYY'
+type ValidDateFormats = 'YYYY-MM' | 'YYYY-MM-DD' | "MMM-DD" | "MMM-YYYY" |"MMM-DD-YYYY" | 'MMMM-YYYY' | 'MMM-YY'
 
 type FormattedDates = {
     [key in ValidDateFormats]: string
@@ -19,14 +19,16 @@ export const convertDate = (inputDate: string | undefined | null = undefined) =>
 
     const getFormattedDates = () => {
         const year = date.getFullYear()
+        const yearShort = String(year).slice(-2)
         const month = date.getMonth()
         const monthNumber = String(month + 1).padStart(2, '0')
         const day = String(date.getDate()).padStart(2, '0')
 
         return {
             'MMM-DD': `${shortMonthNames[month]} ${day}`,
-            'MMM-YYYY': `${shortMonthNames[month]} ${year}`,
             'MMM-DD-YYYY': `${shortMonthNames[month]} ${day}, ${year}`,
+            'MMM-YY' : `${shortMonthNames[month]} ${yearShort}`,
+            'MMM-YYYY': `${shortMonthNames[month]} ${year}`,
             'MMMM-YYYY': `${fullMonthNames[month]} ${year}`,
             'YYYY-MM': `${year}-${monthNumber}`,
             'YYYY-MM-DD': `${year}-${monthNumber}-${day}`,
