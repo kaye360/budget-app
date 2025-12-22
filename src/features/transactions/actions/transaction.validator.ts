4a1a514624d76b310ac701e81b1f43b37ad23d65
joshkaye88 <jkaye@shaw.ca>
2025-12-21 22:29:14 -0700

    Dashboard date prod test

--- src/features/transactions/actions/transaction.validator.ts
+++ src/features/transactions/actions/transaction.validator.ts
@@ -8,7 +8,7 @@ export const TransactionValidator =  {
 
     index : z.object({
         filter : z.string(),
-        filterValue : z.union([ z.string(), z.record(z.any()) ]).optional(),
+        filterValue : z.any().optional(),
         page : z.number().optional(),
         perPage : z.number().optional()
     }),
