e5518496bb91b743e3bea54b0959d1ea226c7043
joshkaye88 <jkaye@shaw.ca>
2026-02-07 21:28:48 -0700

    Add income progress bar to MonthlySpendingSummary

--- src/features/budgets/summaries.actions.ts
+++ src/features/budgets/summaries.actions.ts
@@ -10,6 +10,52 @@ const userId = 1
 
 export const summary = {
 
+    getMonthlyIncome : defineAction({
+        input : z.object({
+            date : z.string().nullable()
+        }),
+        handler : async ({date}) => {
+
+            if( !date ) {
+                date = convertDate().to('YYYY-MM')
+            }
+
+            /**
+             * Transaction data for current month
+             */
+            const {startDate, endDate} = summary.getDateRange(date)
+
+            const { data: transactions } = await db.from('TransactionView')
+                .select('amount')
+                .order('date', { ascending : false })
+                .eq('userId', userId)
+                .eq('isDeleted', false)
+                .eq('type', 'income')
+                .gte('date', startDate)
+                .lte('date', endDate)
+
+            /**
+             * Total income in current month
+             */
+            const currentIncome = Math.round( transactions?.reduce(
+                (acc, current) =>  acc + (current.amount || 0),
+                0
+            ) || 0) 
+
+            const { data : budgets } = await db.from('Budgets')
+                .select('amount')
+                .order('name')
+                .eq('userId', userId)
+                .ilike('name', '%Income%')
+
+            return {
+                total : budgets?.[0]?.amount || 0,
+                current : currentIncome
+            }
+            
+        }
+    }),
+
     getMonthlySpending : defineAction({
         input : z.object({
             date : z.string().nullable()
@@ -23,9 +69,7 @@ export const summary = {
             /**
              * Transaction data for current month
              */
-            const [year, month] = date.split('-').map(Number)
-            const startDate = `${year}-${month}-01`
-            const endDate = (new Date(year, month, 0)).toISOString().slice(0,10)
+            const {startDate, endDate} = summary.getDateRange(date)
         
             const { data: transactions } = await db.from('TransactionView')
                 .select('amount')
@@ -68,5 +112,12 @@ export const summary = {
             }
 
         }
-    })
+    }),
+
+    getDateRange : (date: string) => {
+        const [year, month] = date.split('-').map(Number)
+        const startDate = `${year}-${month}-01`
+        const endDate = (new Date(year, month, 0)).toISOString().slice(0,10)    
+        return { startDate, endDate }
+    }
 }
